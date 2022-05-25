4a590030dbe4f402cf2816f9f6d7c61332e0505d
NatiTesfay <netaneltes66@gmail.com>
2022-05-25 09:08:38 +0300

    23.05.2022--j.s

---
+++ main.js
@@ -0,0 +1,62 @@
+// function myDate() {
+//     var myDate = new Date();
+//     console.log(myDate);
+// }
+// myDate();
+
+// function myDate() {
+//     var myDate = new Date('23.05.2022');
+//     console.log(myDate);
+// }
+// myDate();
+
+// function myDate() {
+//     var myDate = new Date+prompt();
+//     console.log(myDate);
+// }
+// myDate();
+
+// function myDate() {
+//     var someDate = new Date(prompt('enter date'));
+//     console.log(someDate.getMonth());
+//     return someDate.getMonth();
+// }
+// myDate();
+// function myDate(someDate) {
+//     var someDate = new Date(prompt('enter date'));
+//     return someDate.getMonth();
+// }
+// console.log(myDate());
+
+
+// function myDate(someDate) {
+//     var someDate = new Date(prompt('enter date'));
+//     return someDate.getHours();
+// }
+// console.log(myDate());
+
+// function myDate() {
+//     var someDate = new Date(prompt('enter date'));
+//     return someDate.getFullYear();
+// }
+// console.log(myDate());
+
+// function myDate() {
+//     var someDate = new Date(prompt('enter date'));
+//     return someDate.getMinutes();
+// }
+// console.log(myDate());
+
+
+
+// function someRandomDate() {
+//     var rndMonth = Math.floor(Math.random()*12);
+//     var rmdDay = Math.floor(Math.random()*32);
+//     var rndYear = Math.floor(Math.random()*(2023-1990)+1990);
+//     var dateStr = rndMonth + "/" + rmdDay + "/" + rndYear;
+//     var date = new Date(dateStr);
+//     return dateStr;
+// }
+// someRandomDate();
+
+
